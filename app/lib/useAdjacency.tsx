076216b706977d227e57190ca17bd3c56228dd8e
Gregory Huber <greg@ruboss.com>
2023-05-11 17:41:03 -0600

    success state

--- app/lib/useAdjacency.tsx
+++ app/lib/useAdjacency.tsx
@@ -42,9 +42,78 @@ function useAdjacency(adjacencies: number[][]) {
     return edges;
   }, [points, adjacencies])
 
+  const hasCrossings = useMemo(() => {
+    // Function to check if two line segments intersect
+    function doSegmentsIntersect(seg1: number[], seg2: number[]) {
+      // Extract coordinates of the line segments
+      const [x1, y1, x2, y2] = seg1;
+      const [x3, y3, x4, y4] = seg2;
+
+      // Calculate orientations of the line segments
+      const orientation1 = calculateOrientation(x1, y1, x2, y2, x3, y3);
+      const orientation2 = calculateOrientation(x1, y1, x2, y2, x4, y4);
+      const orientation3 = calculateOrientation(x3, y3, x4, y4, x1, y1);
+      const orientation4 = calculateOrientation(x3, y3, x4, y4, x2, y2);
+
+      // Check if the orientations are different
+      if (orientation1 !== orientation2 && orientation3 !== orientation4) {
+        return true; // Segments intersect
+      }
+
+      return false; // Segments do not intersect
+    }
+
+    // Function to calculate the orientation of three points
+    function calculateOrientation(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number) {
+      const val = (y2 - y1) * (x3 - x2) - (x2 - x1) * (y3 - y2);
+      if (val === 0) {
+        return 0; // Collinear points
+      } else if (val > 0) {
+        return 1; // Clockwise orientation
+      } else {
+        return 2; // Counterclockwise orientation
+      }
+    }
+
+    // Function to check for intersection in a graph
+    function checkIntersection(coordinates: vec.Vector2[], adjacencyMatrix: number[][]) {
+      // Iterate through the adjacency matrix
+      for (let i = 0; i < adjacencyMatrix.length; i++) {
+        for (let j = i + 1; j < adjacencyMatrix[i].length; j++) {
+          // Check if there is an edge between vertices i and j
+          if (adjacencyMatrix[i][j] === 1) {
+            // Define line segments using the coordinates
+            const seg1 = coordinates[i].concat(coordinates[j]);
+
+            // Iterate through the remaining edges
+            for (let k = 0; k < adjacencyMatrix.length; k++) {
+              for (let l = k + 1; l < adjacencyMatrix[k].length; l++) {
+                // Check if there is an edge between vertices k and l
+                if (adjacencyMatrix[k][l] === 1 && k !== i && l !== j && l !== i && k !== j) {
+                  // Define line segments using the coordinates
+                  const seg2 = coordinates[k].concat(coordinates[l]);
+
+                  // Check if the line segments intersect
+                  if (doSegmentsIntersect(seg1, seg2)) {
+                    // Add the intersecting edges to the result
+                    return true
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      return false;
+    }
+    return checkIntersection(points, adjacencies)
+  }, [points, adjacencies])
+
   return {
     elements,
-    connections
+    connections,
+    hasCrossings
   }
 }
 
--- app/routes/planar.tsx
+++ app/routes/planar.tsx
@@ -4,6 +4,7 @@ import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { adjacencies } from "~/lib/adjacencies";
 import { useAdjacency } from "~/lib/useAdjacency";
+import { Fireworks } from "fireworks/lib/react"
 
 export function loader({request}: LoaderArgs) {
   const params = new URL(request.url).searchParams;
@@ -18,17 +19,41 @@ export function loader({request}: LoaderArgs) {
   const options = adjacencies[number];
   const choice = Math.floor(Math.random() * options.length)
 
-  return json(options[choice])
+  return json({
+    matrix: options[choice],
+    number: number
+  })
 }
 
 export default function Planar() {
   const data = useLoaderData<typeof loader>();
-  const {elements, connections} = useAdjacency(data)
+  const {elements, connections, hasCrossings} = useAdjacency(data.matrix);
 
   return (
-    <Mafs pan={false} height={600} width={600}>
-      {elements}
-      {connections}
-    </Mafs>
+    <div className="grid grid-cols-1 place-items-center space-y-4">
+      <form method="get" className="space-x-4">
+        <button type="submit" name="number" value="4" className="px-6 py-1 bg-purple-500 rounded-lg">4</button>
+        <button type="submit" name="number" value="5" className="px-6 py-1 bg-purple-500 rounded-lg">5</button>
+        <button type="submit" name="number" value="6" className="px-6 py-1 bg-purple-500 rounded-lg">6</button>
+        <button type="submit" name="number" value={data.number} className="px-6 py-1 bg-purple-500 rounded-lg">New Puzzle</button>
+      </form>
+      <Mafs pan={false} height={600} width={600}>
+        {elements}
+        {connections}
+      </Mafs>
+      {!hasCrossings ?
+        <>
+          <Fireworks count={3} interval={1000} calc={(props, i) => ({
+            ...props,
+            x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
+            y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+          })} />
+          <form method="get">
+            <button type="submit" name="number" value={data.number} className="px-6 py-1 bg-purple-500 rounded-lg outline-purple-300">Next Puzzle</button>
+          </form>
+        </>
+        : null
+      }
+    </div>
   )
 }
